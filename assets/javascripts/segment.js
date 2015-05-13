4d693c13b855aa24dfc62630d7c20689eee6fc9f
0065paula <0065paula@gmail.com>
2015-05-13 17:39:54 +0800

    [gh-pages] add more sass cases

--- assets/javascripts/segment.js
+++ assets/javascripts/segment.js
@@ -3,8 +3,24 @@ $('#logIn').click(function(event) {
     analytics.track('Logged In', {
     username: userNameValue
   });
+    $('#createProject').removeAttr('disabled');
 });
 
 $('#createProject').click(function(event) {
     analytics.track('Create Project');
+    $('#invite').removeAttr('disabled');
+    $('#sdk').removeAttr('disabled');
+});
+
+$('#invite').click(function(event) {
+    analytics.track('Invite User');
+});
+
+$('#sdk').click(function(event) {
+    analytics.track('Install SDK');
+    $('#sendData').removeAttr('disabled');
+});
+
+$('#sendData').click(function(event) {
+    analytics.track('Send Data');
 });
